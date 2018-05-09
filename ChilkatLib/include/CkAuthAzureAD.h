7a78aa645c195091b0240c256f31c10b4152d299
Chilkat Software, Inc <admin@chilkatsoft.com>
2018-05-09 10:37:39 -0500

    Updated for v9.5.0.73

--- ChilkatLib/include/CkAuthAzureAD.h
+++ ChilkatLib/include/CkAuthAzureAD.h
@@ -2,7 +2,7 @@
 //
 //////////////////////////////////////////////////////////////////////
 
-// This header is generated for Chilkat 9.5.0.69
+// This header is generated for Chilkat 9.5.0.73
 
 #ifndef _CkAuthAzureAD_H
 #define _CkAuthAzureAD_H
--- ChilkatLib/include/CkAuthUtilW.h
+++ ChilkatLib/include/CkAuthUtilW.h
@@ -2,7 +2,7 @@
 //
 //////////////////////////////////////////////////////////////////////
 
-// This header is generated for Chilkat 9.5.0.69
+// This header is generated for Chilkat 9.5.0.73
 
 #ifndef _CkAuthUtilW_H
 #define _CkAuthUtilW_H
